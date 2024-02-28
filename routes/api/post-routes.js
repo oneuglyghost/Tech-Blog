2bf5a04e0e6129aa9dc28a8c376cd4650af9c585
Bernardo Alonso <bernardo4430@gmail.com>
2024-02-28 14:21:26 -0600

    made routes for creating post and deleting them

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,9 +5,30 @@ const {authenticateToken} = require('../../middleware/auth');
 
 
 router.post("/create", authenticateToken, async (req, res) => {
-    // add to create new post 
+    // create new post 
+    try {
+        const {title, content} = req.body;
+        const newpost = await post.create({title, content });
+        res.status(201).json(newPost);
+    }catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Failed to create post" });
+    }
 });
 
-// any other route like delete route
+//  delete  a post route
+router.delete("./id", authenticateToken, async (req, res) => {
+    try {
+        const postId =req.params.id;
+        const deletedPost = await Post.destroy({ where: { id: postId } });
+        if (!deletedPost) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+        res.status(200).json({ message: 'Post deleted successfully'})
+    }catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to delete post' });
+    }
+})
 
 module.exports = router;
